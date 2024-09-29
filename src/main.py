010dafc723602acb80f7990602bdf94516e85701
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2024-09-29 07:02:31 -0400

    Added commented code for intergration

--- src/main.py
+++ src/main.py
@@ -2,12 +2,21 @@ from flask import Flask
 from flask_restful import Api, Resource, abort
 
 import scraper
+from src.hobby_extraction import create_pipeline
+
+pipeline = create_pipeline()
 
 app = Flask(__name__)
 api = Api(app)
 
 class hobby_card(Resource):
 	def get(self, hobby, location):
+		global pipeline
+
+		# scrapper (downloads reels)
+
+		# hobby = pipeline([all files downloaded])
+
 		pre_result = scraper.scraper(location, hobby)
 		result = {"hobby": hobby, "name": pre_result[0], "location": pre_result[1], "description": pre_result[2], "image": pre_result[3], "url": pre_result[4]}
 		if not result:
